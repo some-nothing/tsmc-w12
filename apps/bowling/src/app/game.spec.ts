9f702dab01d31439a4e7fb3fc8940303dc214ff5
hslin <linroex@coder.tw>
2022-05-04 12:45:07 +0000

    1, red

---
+++ apps/bowling/src/app/game.spec.ts
@@ -0,0 +1,17 @@
+import { Game } from './game';
+
+describe('Game', () => {
+  it('should create an instance', () => {
+    expect(new Game()).toBeTruthy();
+  });
+  
+  test('gutter game', () => {
+    const game = new Game();
+    for (let i = 0; i < 20; i++) {
+      game.roll(0);
+    }
+    expect(game.score).toBe(0);
+  });
+
+});
+
